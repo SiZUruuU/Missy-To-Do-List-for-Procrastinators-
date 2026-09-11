805a4e40a8894016d4830cd79d4358c6f5b4a103
SiZUruuU <savagefeeble@gmail.com>
2026-09-11 20:50:43 +0800

    Added Mouse Handler class

---
+++ Components/Utilities/MouseHandler.java
@@ -0,0 +1,68 @@
+package Components.Utilities;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.MouseInputListener;
+
+public class MouseHandler implements  MouseInputListener{
+
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+
+        int x = e.getX();
+        int y = e.getY();
+
+    }
+    
+}
